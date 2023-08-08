da9ace1d885bcb65143a0b92b04d25d63bec3dc8
Danilo Matrangolo Marano <danilo.marano@mac.com.br>
2023-08-08 19:56:55 -0300

    Tradução de texto inglês para francês

--- back/app/utils.py
+++ back/app/utils.py
@@ -1,6 +1,5 @@
 """utils.py
 
-
 ÚTEIS
 --------------------------------------------------------------------------------
 Funções utilitárias, que podem ser usadas em várias partes do código.
@@ -18,4 +17,11 @@ def text_generation(text):
     generator = pipeline("text-generation")
     res = generator(text, max_length=100, num_return_sequences=2)
     return res
+
+
+def translation(text):
+    translator = pipeline("translation_en_to_fr")
+    res = translator(text)
+    return res
+
 
